cf5c0a4bd414c4adfe70a4994c47565b388f6b81
Roland Benedetti <rolandbenedetti@pantheon.io>
2026-04-22 11:01:51 -0400

    Update test message

--- components/test.tsx
+++ components/test.tsx
@@ -1,6 +1,6 @@
   import { ComponentConfig } from "@puckeditor/core";
   export const TestBlock: ComponentConfig = {
     fields: { text: { type: "text" } },
-    defaultProps: { text: "Hello from submodule!" },
+    defaultProps: { text: "Hello from submodule rev 2`!" },
     render: ({ text }) => <div className="p-4 bg-yellow-100">{text}</div>,
   };
